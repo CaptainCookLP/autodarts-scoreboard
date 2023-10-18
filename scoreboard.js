2f05d755a834ddc0fff48d4a59b168988fd17f16
FKmedify | Fabi <67053133+CaptainCookLP@users.noreply.github.com>
2023-10-18 15:12:21 +0200

    Add files via upload

--- scoreboard.js
+++ scoreboard.js
@@ -1,7 +1,12 @@
-const uri = 'ws://172.28.155.25:8079';
+const uri = 'ws://172.20.230.254:8079';
 const ws = new WebSocket(uri);
 const current = document.getElementById("current");
 const statsTable = document.getElementById("stats_table");
+
+document.getElementById('cssDropdown').addEventListener('change', function() {
+    var selectedCSS = document.getElementById('cssDropdown').value;
+    document.getElementById('selectedCSS').setAttribute('href', selectedCSS);
+});
 
 function animateCountdown(elementId, startValue, endValue, duration) {
   const element = document.getElementById(elementId);
@@ -38,6 +43,17 @@ function toggleVisibility(isVisible) {
   checkoutContainer.style.right = isVisible ? '705px' : '620px';
 }
 
+function calculateSetAverages(p1Stats, p2Stats, p1Sets, p2Sets) {
+  if (p1Sets < 1 || p2Sets < 1) {
+    return { p1AverageSet: 0, p2AverageSet: 0 };
+  }
+
+  const p1AverageSet = p1Stats.average / p1Sets;
+  const p2AverageSet = p2Stats.average / p2Sets;
+
+  return { p1AverageSet, p2AverageSet };
+}
+
 function updateGameData(dataJson) {
   const currentPlayer = dataJson.player;
   const checkoutGuide = dataJson.state ? dataJson.state.checkoutGuide : null;
@@ -51,10 +67,20 @@ function updateGameData(dataJson) {
     scores: [p1Scores, p2Scores],
     stats: [p1Stats, p2Stats],
   } = dataJson;
+  
+  const { p1AverageSet, p2AverageSet } = calculateSetAverages(
+    p1Stats,
+    p2Stats,
+    p1Scores.sets,
+    p2Scores.sets
+  );
+
+  updateElementValue('p1_average_set', formatPPR(p1AverageSet));
+  updateElementValue('p2_average_set', formatPPR(p2AverageSet));
 
   const [p1, p2] = [p1Data.name, p2Data.name].map(name => name.toUpperCase());
 
-  updateElementValue('game', `First to ${sets ? `${sets} Sets / ${legs} Legs` : `${legs} Legs`}`);
+  updateElementValue('game', `${sets ? `${sets} Sets / ${legs} Legs` : `${legs} Legs`}`);
   //toggleVisibility(sets === null);
   animateCountdown('p1_left', parseInt(document.getElementById('p1_left').textContent), p1_left, 1000);
   animateCountdown('p2_left', parseInt(document.getElementById('p2_left').textContent), p2_left, 1000);
@@ -67,6 +93,8 @@ function updateGameData(dataJson) {
   updateElementValue('p2_sets', p2Scores.sets);
   updateElementValue('p1_ppr', formatPPR(p1Stats.average));
   updateElementValue('p2_ppr', formatPPR(p2Stats.average));
+  updateElementValue('p1_set1', formatPPR(p1AverageSet));
+  updateElementValue('p2_set1', formatPPR(p2AverageSet));
   ['100', '140', '60', '180'].forEach(type => {
     updateElementValue(`p1_${type}`, type === '180' ? p1Stats[`total${type}`] : p1Stats[`plus${type}`]);
     updateElementValue(`p2_${type}`, type === '180' ? p2Stats[`total${type}`] : p2Stats[`plus${type}`]);
